4710fcc5ef042588b3ad66113f4fb7cbd0036dd1
jimmycourtice <jimmy.courtice@gmail.com>
2022-05-09 22:41:40 -0500

    Adding Context Stuff

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,8 +1,10 @@
+import { useContext } from 'react';
 import {motion, AnimatePresence} from 'framer-motion'
-import PropTypes from 'prop-types'
 import FeedbackItem from "./FeedbackItem";
+import FeedbackContext from '../context/FeedbackContext';
 
-function FeedbackList({feedback, handleDelete}) {
+function FeedbackList() {
+    const {feedback} = useContext(FeedbackContext)
     if (!feedback || feedback.length === 0) {
         return <p>No Feedback Yet.</p>
     }
@@ -17,8 +19,8 @@ function FeedbackList({feedback, handleDelete}) {
                     exit={{opacity: 0}}>
                 <FeedbackItem 
                 key={item.id} 
-                item={item} 
-                handleDelete={handleDelete}/>
+                item={item}
+                />
                 </motion.div>
             ))}
             </AnimatePresence>
@@ -34,16 +36,6 @@ function FeedbackList({feedback, handleDelete}) {
     //         ))}
     //     </div>
     // )
-}
-
-FeedbackList.propTypes = {
-    feedback: PropTypes.arrayOf(
-        PropTypes.shape({
-           id: PropTypes.number.isRequired,
-           text: PropTypes.string.isRequired,
-           rating: PropTypes.number.isRequired,
-        })
-    )
 }
 
 export default FeedbackList
